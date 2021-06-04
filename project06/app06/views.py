551cc427b723d31a7c38d0291e35cceaf4da3034
siddhartha <siddhu>
2021-06-04 14:57:49 +0530

    example6

---
+++ project06/app06/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def showIndex(request):
+    students_info = {"data":
+                         [
+                             {"idno":101,"name":"siddhartha","marks":[65,35,55,65,73,40]},
+                             {"idno":102,"name":"ranjith","marks": [60,65,75,77,45,60]},
+                             {"idno":103,"name": "tharun","marks": [68,54,67,87,44,65]},
+                             {"idno":104,"name": "imran","marks": [35,45,66,76,36,"ab"]},
+                             {"idno":105,"name": "akhilesh","marks": [66,76,35,49,57,82]},
+                         ]
+                    }
+    return render(request,"marks.html",students_info)
